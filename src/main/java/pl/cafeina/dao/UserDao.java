2ff780f41b1ab378dcbb96d5b655b566649c2a65
WaclawZ <waclaw.zalicki@gmail.com>
2018-01-24 15:37:15 +0100

    Added User entity and dao

---
+++ src/main/java/pl/cafeina/dao/UserDao.java
@@ -0,0 +1,11 @@
+package pl.cafeina.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.cafeina.entity.User;
+
+import java.util.List;
+
+public interface UserDao extends JpaRepository<User, Long> {
+
+    
+}
---
+++ src/main/java/pl/cafeina/entity/User.java
@@ -0,0 +1,105 @@
+package pl.cafeina.entity;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Size(min = 3)
+    private String firstName;
+
+    @Size(min = 3)
+    private String lastname;
+
+    @Email
+    private String email;
+
+    @Size(min = 5)
+    private String password;
+
+    @Pattern(regexp = "^[0-9]{6,9}$")
+    private String phoneNumber;
+
+    @NotEmpty
+    private Integer age;
+
+    @NotNull
+    private Boolean admin = false;
+
+    public User(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }
+}
